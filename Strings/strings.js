2999303e9e499b6a5c151ec2d0a77021d38affb4
Anderson Suassuna <andersonssgf7@gmail.com>
2024-03-10 16:15:07 -0300

    Create strings.js

---
+++ Strings/strings.js
@@ -0,0 +1,19 @@
+const escola = "Cod3r"
+
+console.log(escola.charAt(4)) // Achar caractere
+
+console.log(escola.charAt(5))
+
+console.log(escola.charCodeAt(3)) // Achar código do caractere
+
+console.log(escola.indexOf("3")) // Achar posição de um caractreze
+
+console.log(escola.substring(0, 3)) // Printar string de um caractere a outro
+
+console.log("Escola ".concat(escola).concat("!")) // Concatenar 1
+
+console.log("Escola " + escola + "!") // Concatenar 2
+
+console.log(escola.replace(3, "e")) // Substituir partes da string
+
+console.log("Ana,Maria,Pedro".split(",")) // Separar os valores para um array
