d989c6506fe9f9ff4e3b1da5ee451dce0b9dceb4
Anderson Suassuna <andersonssgf7@gmail.com>
2024-03-10 18:10:43 -0300

    Create objetos.js

---
+++ Aulas/objetos.js
@@ -0,0 +1,11 @@
+const prod1 = {} // Um objeto é definido por chaves -> {}
+prod1.nome = "Celular Ultra Mega" // Um objeto armazena uma chave (identificador) e valor
+prod1.preco = 5000.00 // Evitar atributos com espaço
+
+console.log(prod1)
+
+const prod2 = { // Em um bloco fica muito mais organizado
+    nome: "Camisa Polo",
+    preço: 80.00
+}
+console.log(prod2)
